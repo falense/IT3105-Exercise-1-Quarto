f1e7f2d5086a3ab60c7d2c90738546dca5b8459f
Sondre Engebraten <sondree@stud.ntnu.no>
2013-09-15 21:11:32 +0200

    Update statisticsrunner to be able to run an arbitrary number of matches

--- src/StatisticsRunner.java
+++ src/StatisticsRunner.java
@@ -17,52 +17,10 @@ public class StatisticsRunner {
 		this.p2 = p2;
 		this.numMatches = numMatches;
 	}
-	public int [] simulate(boolean reverse){
+	public int[] doGames(BasePlayer p1, BasePlayer p2, int matchesToPlay){
 		ArrayList<GM> gameMasters = new ArrayList<GM>();
 		ArrayList<Thread> threads = new ArrayList<Thread>();
-		for (int i = 0; i < numMatches; i++){
-			GM g;
-			if (!reverse)
-				g = new GM(false,false,0,p1,p2);
-			else 
-				g = new GM(false,false,0,p2,p1);
-			Thread t = new Thread(g, "Quarto " + i);
-			t.start();
-			gameMasters.add(g);
-			threads.add(t);
-		}
-
-		int results[] = new int[3];
-		for (int i = 0; i < 3; i++) results[i] = 0;
-		for (int i = 0; i < numMatches; i++){
-			//System.out.println("Complete " + ((double)i / (double)numMatches) + "%");
-			try {
-				threads.get(i).join();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			int index = gameMasters.get(i).winner;
-			results[index]++;
-		}
-		System.out.println(numMatches + " games was played, the results are:");
-		if (!reverse){
-			System.out.println(p1.getName() + " won: " + results[1]);
-			System.out.println(p2.getName() + " won: " + results[2]);
-		}
-		else{
-			System.out.println(p1.getName() + " won: " + results[2]);
-			System.out.println(p2.getName() + " won: " + results[1]);
-			
-		}
-		System.out.println("Draws: " + results[0] + "\n" );
-		return results;
-
-	}
-	public int[] doGames(BasePlayer p1, BasePlayer p2, int numMatches){
-		ArrayList<GM> gameMasters = new ArrayList<GM>();
-		ArrayList<Thread> threads = new ArrayList<Thread>();
-		for (int i = 0; i < batchSize; i++){
+		for (int i = 0; i < matchesToPlay; i++){
 			GM g = new GM(false,false,0,p1,p2);
 			Thread t = new Thread(g, "Quarto " + i);
 			t.start();
@@ -72,7 +30,7 @@ public class StatisticsRunner {
 
 		int results[] = new int[3];
 		for (int i = 0; i < 3; i++) results[i] = 0;
-		for (int i = 0; i < batchSize; i++){
+		for (int i = 0; i < matchesToPlay; i++){
 			try {
 				threads.get(i).join();
 			} catch (InterruptedException e) {
@@ -86,37 +44,44 @@ public class StatisticsRunner {
 	}
 	public void run(){
 		int batchesOfMatches = numMatches/batchSize;
+		int matchCount = 0;
 		int []totalScore = new int[3];
 		for (int j = 0; j < 3; j++){
 			totalScore[j] += 0;
 		}
-		for (int i = 0; i < batchesOfMatches; i++){
-			System.out.println("Complete " + (((double)i*100)/((double)batchesOfMatches)) + "%");
+		for (int i = 0; i < batchesOfMatches+1; i++){
+			System.out.println("Complete " + (((double)matchCount*100)/((double)numMatches)) + "%");
 			int []r;
+			int matchesToPlay = min(batchSize,numMatches-matchCount);
 			if (i%2 == 0){
-				r = doGames(p1, p2, 10);
+				r = doGames(p1, p2, matchesToPlay);
 
 				for (int j = 0; j < 3; j++){
 					totalScore[j] += r[j];
-					//System.out.println(totalScore[j] + " += " + r[j] );
 				}
 			}
 			else{
-				r = doGames(p2, p1, 10);
+				r = doGames(p2, p1, matchesToPlay);
 				totalScore[0] += r[0];
 				totalScore[2] += r[1];
 				totalScore[1] += r[2];
 			}
+			matchCount += min(batchSize,numMatches-matchCount);
 		}
-		System.out.println(numMatches + " games was played, the results are:");
+		System.out.println(matchCount + " games was played, the results are:");
 
 		System.out.println(p1.getName() + " won: " + totalScore[1]);
 		System.out.println(p2.getName() + " won: " + totalScore[2]);
 		System.out.println("Draws: " + totalScore[0] + "\n" );
 	}
+	private int min(int x, int y) {
+		// TODO Auto-generated method stub
+		if (x < y) return x;
+		else return y;
+	}
 	public static void main(String[] args)
     {
-		StatisticsRunner s2 = new StatisticsRunner( new MinMaxAI(false,1),new RandomAI(false), 100);
+		StatisticsRunner s2 = new StatisticsRunner( new MinMaxAI(false,1),new RandomAI(false), 99);
 		s2.run();
 	}
 }
